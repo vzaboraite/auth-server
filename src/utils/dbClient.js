0b5ed58d023a702bf50ada9904716f254b7f0602
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-22 23:03:47 +0000

    Update dbClient.js with query logs

--- src/utils/dbClient.js
+++ src/utils/dbClient.js
@@ -1,5 +1,16 @@
-const { PrismaClient } = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client");
 
-const dbClient = new PrismaClient()
+const dbClient = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+  ],
+});
+
+prisma.$on("query", async (e) => {
+  console.log(e);
+});
 
 module.exports = dbClient;
