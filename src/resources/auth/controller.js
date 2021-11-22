ffd0dc29c257c5307eb814e77bf02b3a51b7a191
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-22 23:27:28 +0000

    Create `signup` controller w\ validation

--- src/resources/auth/controller.js
+++ src/resources/auth/controller.js
@@ -1,4 +1,22 @@
 const dbClient = require("../../utils/dbClient");
 const prisma = dbClient;
 
-module.exports = {};
+const signup = async (req, res) => {
+  console.log("Inside signup controller: ", req.body);
+  const userToCreate = { ...req.body };
+
+  try {
+    const result = await prisma.user.create({
+      data: {
+        ...userToCreate,
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    console.error("[ERROR] /signup route: ", error);
+
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = { signup };
--- src/resources/auth/router.js
+++ src/resources/auth/router.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const { signup } = require("./controller");
 
 const router = Router();
+
+router.post("/signup", signup);
 
 module.exports = router;
