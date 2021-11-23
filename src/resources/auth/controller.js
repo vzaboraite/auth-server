52e0b878caf4b5bf745e493d3dfab5a4457e10f1
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-23 21:50:44 +0000

    Build `signin` controller

--- src/resources/auth/controller.js
+++ src/resources/auth/controller.js
@@ -19,4 +19,34 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res) => {
+  console.log("Inside signin controller: ", req.body);
+  const userToFind = {
+    ...req.body,
+  };
+  try {
+    const foundUser = await prisma.user.findUnique({
+      where: {
+        email: userToFind.email,
+      },
+    });
+
+    if (!foundUser) {
+      res.status(401).json({ error: "Authentication failed" });
+    }
+
+    if (userToFind.password === foundUser.password) {
+      res.status(200).json(foundUser);
+    } else {
+      res.status(401).json({ error: "Authentication failed" });
+    }
+
+    console.log(foundUser);
+  } catch (error) {
+    console.error("[ERROR] /signin route: ", error);
+
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = { signup, signin };
--- src/resources/auth/router.js
+++ src/resources/auth/router.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { signup } = require("./controller");
+const { signup, signin } = require("./controller");
 
 const router = Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
